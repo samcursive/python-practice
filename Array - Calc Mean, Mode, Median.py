be8968c6ebf6276297abd59d0e5f040fbeecd946
Sam Arshad <jsb211@txstate.edu>
2025-05-13 11:32:39 -0500

    Array.py: Mean - Calculated mean using Numpy. Did it two ways. Will document more ways to find mean with and without existing functions.

--- Array - Calc Mean, Mode, Median.py
+++ Array - Calc Mean, Mode, Median.py
@@ -1,5 +1,9 @@
 # Calculate Mean, Mode and Median Given an Array
 
+import numpy
+
+
+
 Numbers = [10,15, 23, 45, 50, 971]; n = len(Numbers); avg = 0; sum = 0
 
 for i in Numbers:
@@ -9,5 +13,9 @@ avg = sum / n
 
 print(f"The Average of given array is: {avg:.2f}")
 
+# Using numpy
+print(f"Using Numpy to do the work for me: {numpy.mean(Numbers):.2f}.")
 
 
+
+#Numbers
